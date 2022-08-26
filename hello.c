733a0a364b0ab71c7a860e2e6f315b352abab2f2
Carter Fleshman <carter.fleshman@huskers.unl.edu>
2022-08-26 01:58:20 +0000

    added my name too

--- hello.c
+++ hello.c
@@ -2,10 +2,15 @@
  *
  */
 
+#include<stdlib.h>
+#include<stdio.h>
+
 int main(int argc, char **argv) {
 
     printf("Hello, I'm Nathan Dubas!\n");
     printf("Major is Computer Engineering\n");
+    printf("Hello my name is Carter Fleshman!\n");
+    printf("Major is Mechanical Engineering\n");
 
     return 0;
 }
